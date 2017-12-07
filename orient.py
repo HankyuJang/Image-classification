719846830f0e355a22aef4e29145910aea26e701
Pulkit Maloo <maloop@iu.edu>
2017-12-07 01:44:51 -0500

    76% Accuracy for NN

--- orient.py
+++ orient.py
@@ -25,9 +25,8 @@
 # ==  Neural Network  =========================================================
 #
 # Implemented He Initialization
-# Implemented Regularization
-# Sometimes gives divide by 0 error for certain values of lambda
-# Softmax
+# Implemented L2 Regularization
+# ReLU for hidden layers and Softmax for output layer
 #
 ##### TODO  ###############
 #
@@ -39,15 +38,15 @@ import sys
 import timeit
 import numpy as np
 from numpy.linalg import norm
+from sklearn.preprocessing import LabelBinarizer
 import cPickle
 from collections import Counter
 from heapq import nsmallest
-from random import shuffle
-
+import math
 np.random.seed(3)
 
 class NeuralNet(object):
-    def __init__(self, alpha=0.3, iterations=1000, lambd=0, layer_dims=[192, 16, 4]):
+    def __init__(self, alpha=0.3, iterations=4000, lambd=0.7, layer_dims=[192, 128, 64, 4]):
         self.set_parameters(alpha, iterations, lambd, layer_dims)
 
     def set_parameters(self, alpha, iterations, lambd, layer_dims):
@@ -155,10 +154,6 @@ class NeuralNet(object):
         elif activation == "softmax":
             t = self.softmax(Z)
             dZ = t - Z
-#            dZ = np.zeros(shape=Z.shape)
-#            for i in range(dZ.shape[1]):
-#                if
-#                dZ[:,i] = t[:,i] * (1 - t[:,i])
 
         dA_prev, dW, db = self.linear_backward(dZ, (A_prev, W, b), l)
 
@@ -168,11 +163,10 @@ class NeuralNet(object):
         grads = {}
         L = len(caches)
         Y = Y.reshape(AL.shape)
+        cache = caches[L-1]
 
 #        dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
-
-        cache = caches[L-1]
-#        grads["dA"+str(L)], grads["dW"+str(L)], grads["db"+str(L)] = self.linear_activation_backward(dAL, cache, "softmax", L)
+#        grads["dA"+str(L)], grads["dW"+str(L)], grads["db"+str(L)] = self.linear_activation_backward(dAL, cache, "sigmoid", L)
         grads["dA"+str(L)], grads["dW"+str(L)], grads["db"+str(L)] = self.linear_backward(self.dZL(AL, Y), cache[0], L)
         for l in range(L-1, 0, -1):
             cache = caches[l-1]
@@ -182,6 +176,12 @@ class NeuralNet(object):
             grads["db" + str(l)] = db_temp
 
         return grads
+
+    def lrate(self, epoch):
+        drop = 0.5
+        epochs_drop = 1000
+        lr = self.alpha * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+        return lr
 
     def update_parameters(self, grads):
         L = len(self.layer_dims) - 1
@@ -193,8 +193,8 @@ class NeuralNet(object):
         X = X_train
         Y = y_train
 
-        self.costs = [(0,0)]
-        for i in range(self.iterations):
+        costs = [(0,0)]
+        for i in xrange(self.iterations):
             AL, caches = self.forward_propogation(X)
 
             cost = self.compute_cost(AL, Y, caches)
@@ -203,24 +203,28 @@ class NeuralNet(object):
 
             self.update_parameters(grads)
 
-#            if abs(self.costs[-1][1] - cost) < 10**(-6):
-#                break
-
             if i%100 == 0:
-#                print AL
-#                print Y
-                self.costs.append((i, cost))
-                print "Iteration", i, "-> Cost", cost
+                if i % 1000 == 0 and i < 3000:
+                    self.alpha /= 1.5
+                costs.append((i, cost))
+                acc = self.test(X, Y)
+                print "Iteration", i, "->", "Accuracy", acc, "|| Cost", cost
+
+        self.costs = costs
+        self.caches = caches
+        self.AL = AL
+        self.gradients = grads
 
     def test(self, X_test, y_test):
         X_t = X_test
         Y_t = y_test
-
         AL,_ = self.forward_propogation(X_t)
-        original = Y_t.argmax(0)
-        predicted = AL.argmax(0)
-        incorrect = np.count_nonzero(original - predicted)
-        return round((len(original) - incorrect) / len(original), 2) * 100
+
+        self.original = Y_t.argmax(0)
+        self.predicted = AL.argmax(0)
+        m = len(self.original)
+        incorrect = np.count_nonzero(self.original - self.predicted)
+        return round((m - incorrect) / m, 2) * 100
 
 
 class KNN(object):
@@ -265,30 +269,27 @@ def read_file(fname, shuffle_data=True):
     y = np.loadtxt(fname, usecols=1, dtype=int)  # .reshape(len(X), 1)
 
     if shuffle_data:
-        shuffle_indices = list(range(len(y)))
-        shuffle(shuffle_indices)
+        shuffle_indices = range(len(y))
+        np.random.shuffle(shuffle_indices)
         X  = X[shuffle_indices, ]
         y = y[shuffle_indices, ]
-
 
     return X/255, y
 
 def transform_Y_for_NN(Y):
-#    Y = Y.reshape(len(Y), 1)
-    from sklearn.preprocessing import LabelBinarizer
     lb = LabelBinarizer()
     lb.fit(Y)
     return lb
 
 
 if __name__ == "__main__":
-#    task, fname, model_file, model = sys.argv[1:]
+    task, fname, model_file, model = sys.argv[1:]
 #    task, fname, model_file, model = "train train-data.txt knn.txt nearest".split()
 #    task, fname, model_file, model = "test test-data.txt knn.txt nearest".split()
-    task, fname, model_file, model = "train train-data.txt nnet.txt nnet".split()
-#    task, fname, model_file, model = "test train-data.txt nnet.txt nnet".split()
+#    task, fname, model_file, model = "train train-data.txt nnet.txt nnet".split()
+#    task, fname, model_file, model = "test test-data.txt nnet.txt nnet".split()
 
-    X, y = read_file(fname)
+    X, y = read_file(fname, shuffle_data=False)
 
     REDUCE_DIM = False
 
@@ -314,14 +315,13 @@ if __name__ == "__main__":
             lb = transform_Y_for_NN(y)
             Y_lb = lb.transform(y)
 
-            alpha = 0.1
-            iterations = 1000
-            lambd = 0
-            layers = [X.shape[1]] + [193] + [Y_lb.shape[1]]
+            alpha = 0.3
+            iterations = 4000
+            lambd = 0.7
+            layers = [X.shape[1]] + [128, 64] + [Y_lb.shape[1]]
 
             nnet = NeuralNet(alpha=alpha, iterations=iterations, lambd=lambd, layer_dims=layers)
 
-#            nnet = NeuralNet(layer_dims=layers)
             nnet.train(X.T, Y_lb.T)
 
             models = (lb, nnet)
